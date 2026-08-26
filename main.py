040db9a147223b42c7ec576cb7a2dbdb0c667709
Joel H. <166711728+Artiehen@users.noreply.github.com>
2026-08-26 20:04:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import json
 
 from mazegenerator import MazeGenerator
 
+# class CustomError(Exception):
+#     pass
+
 
 def remove_comments(text) -> str:
     """This function removes comments from config.json for
@@ -36,19 +39,30 @@ def main() -> None:
     if len(sys.argv) != 2:
         print("Please run game as 'python3 pac-man.py config.json'")
         sys.exit(1)
-    else:
-        print("Ok")
 
-    with open("config.json") as f:
-        text = f.read()
+    try:
+        with open("config.json") as f:
+            text = f.read()
 
-    data = json.loads(remove_comments(text))
+        data = json.loads(remove_comments(text))
 
-    print(data)
+        print(data)
+        size = (data["width"], data["height"])
 
-    maze_gen = MazeGenerator()
+        maze_gen = MazeGenerator(size)
+        # maze_gen.generate()
 
-    maze_gen.generate()
+        maze = maze_gen.maze
+
+        with open("test.txt", "w") as e:
+            for row in maze:
+                e.write("".join(f"{cell:X}" for cell in row) + "\n")
+        # display_maze(maze, False)
+
+    except FileNotFoundError:
+        print("Unable to locate config.json")
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
